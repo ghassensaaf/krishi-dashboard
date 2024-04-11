41049d3df1710b2cad364b6f0fa4870a01581c9f
Ghassen Saaf <ghassen.saaf@esprit.tn>
2024-04-12 00:05:59 +0100

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Box, Container } from '@mui/material';
+import HomeComponent from '../components/HomeComponent';
+
+export default function Home() {
+  return (
+    <Box>
+      <Container>
+        <HomeComponent />
+      </Container>
+    </Box>
+  );
+}
